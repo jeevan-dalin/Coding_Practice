ffbb1f84ff8bfe9219879ed7d74d2bb0578c6796
Jeevan Dalin <jeevandalin@Jeevans-MacBook-Air.local>
2025-11-26 10:20:51 +0530

    pracitice nov26th, 2025

--- Apple_practice/merge_sorted_lists.py
+++ Apple_practice/merge_sorted_lists.py
@@ -3,9 +3,3 @@
 # Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
 
 # Return the head of the merged linked list.
-
-arr=[1,2,4,5]
-
-arr.insert(2,3)
-
-print(arr)
--- revision/2sum.py
+++ revision/2sum.py
@@ -1,3 +1,5 @@
+#Revision of two sum problem for the date - 26th Nov.
+
 #Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -5,37 +7,55 @@
 # You can return the answer in any order.
 
 
-# def twosum(arr,k):
+# def two_sum(arr,k):
 #     for i in range(len(arr)-1):
+#         if arr[i]+arr[i+1]==k:
+#             print(i,i+1)
+            
+            
+            
+# arr=[1,2,3,4,5]
+
+# k=6
+
+# two_sum(arr,k)
+
+#the program isnt efficient , becuase it checks for the consequrnt elements for sum , but what if the elements are consiquent,
+# for 6 we need 1+5 , which is 0th element and last element. 
+
+ 
+# def two_sum(arr,k):
+#     for i in range(len(arr)):
 #         for j in range(i,len(arr)):
 #             if arr[i]+arr[j]==k:
 #                 print(i,j)
-#                 point=1
-#         if point==1:
+#                 flag=1 # adding flag to mark the execution of first occurance.
+#         if flag == 1:
 #             break
-        
-        
-        
+
 # arr=[1,2,3,4,5]
 
-# twosum(arr,5)
+# k=6
 
-def twosum(arr,k):
-    # hashmap=dict(enumerate(arr))
-    # print(hashmap)
-    h={} #declaring the dict/hashmap
+# two_sum(arr,k)
+
+# the above solves the problem , but the complexity is O (n^2)
+
+def two_sum(arr,k):
+    hmap={}
+
+    for i in range(len(arr)):
+        hmap[arr[i]]=i
     
-    for i in range(len(arr)): # making value of lists as key for hashmap and index as value to it
-        h[arr[i]]=i     #h[key]=value
-    
-    for i in range(len(arr)): # searching the index where diff value is present. a[i]+a[j]=k
-        y=k-arr[i]
-        if y in h and h[y]!=i:
-            print(i,h[y])
+    for i in range(len(arr)):
+        target=k-arr[i]
+        if target in hmap and hmap[target]!=i:
+            print(i,hmap[k-arr[i]])
             break
-        
-arr=[1,2,3,4,5]
 
-twosum(arr,5)
+arr=[3,2,2,3]
+k=6
+two_sum(arr,k)
 
 
+# the above solution is optmized one , which uses linear call - O(n).
